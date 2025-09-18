a9d4b29363df50c80304e07f3ef093f974b72570
Omar Saber Talaat Abdelrahman <omar.abdelrahman.de@gmail.com>
2025-09-18 20:51:32 +0200

    compile_commands

---
+++ compile_commands.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+##### dependiencies: utils.sh
+
+. "${HOME}/scripts/utils.sh"
+
+##### end of dependencies
+
+#####
+## This file contains common helper compile/run commands for various
+## programming languages.
+##
+## NOTE: To avoid name clashes with other scripts
+## * Functions used outside of this script are prefixed with `cr_cmd_`
+## * Helper functions for this script are prefixed with `cr_cmd_utils_`
+##
+## Please stick to the guidelines
+#####
+
+cr_cmd_rust_competitive_manifest() {
+  cargo run --color=always --bin "$1" --profile dev --manifest-path "$HOME/development/sebres/rust-competitive-programming/tasks/$1/Cargo.toml"
+}
+
+cr_cmd_cpp() {
+  local version="$1"
+  local name="$2"
+
+  _validate_positive_integer version || return 1
+
+  if [[ -f "$name".cpp ]]; then
+    g++ -std=c++"$version" -g -Wall "$name".cpp -o "$name"
+  else
+    echo "File $name does not exist"
+    return 1
+  fi
+}
